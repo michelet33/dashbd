212b5509253a9cd899f3afbc34416883aeebc08d
mta <tcha.michele@gmail.com>
2023-03-02 11:31:42 +0100

    update utils.py, add def save_charger

--- ocpp_16/app/models/utils.py
+++ ocpp_16/app/models/utils.py
@@ -5,7 +5,8 @@ import requests
 # URL = os.getenv("AUTHENTICATION_SERVICE")
 # LOGIN_ENDPOINT = f"{URL}/login/"
 # LOGIN_ENDPOINT = "http://host.docker.internal:8080/login/"
-LOGIN_ENDPOINT = "http://192.168.1.129:5000/log/add/"
+LOGIN_ENDPOINT = "http://192.168.1.129:5000"
+# LOGIN_ENDPOINT = "http://127.0.0.1:5000"
 
 
 async def save_log(data):
@@ -14,7 +15,24 @@ async def save_log(data):
         # the following line is responsible for suppressing the warning.
         requests.packages.urllib3.disable_warnings()
         response = requests.post(
-           url=LOGIN_ENDPOINT,
+           url=f"{LOGIN_ENDPOINT}/log/add/",
+           data= djson,
+           timeout= 30,
+           verify = False
+        )
+
+        response.raise_for_status()
+    except requests.HTTPError as error:
+        print(error)
+
+
+async def save_charger(data):
+    try:
+        djson = json.dumps(data)
+        # the following line is responsible for suppressing the warning.
+        requests.packages.urllib3.disable_warnings()
+        response = requests.post(
+           url=f"{LOGIN_ENDPOINT}/charger/set/",
            data= djson,
            timeout= 30,
            verify = False
